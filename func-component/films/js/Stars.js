4ae4938d43dd08983089b4c2250694e0993b6006
Andrey <34788837+Dremuchev@users.noreply.github.com>
2018-07-12 15:53:41 +0300

    Update Stars.js

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,6 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars(props) {
+  const count = (typeof(props.count) === 'number' && props.count >= 1 && props.count <= 5) ? props.count : null;
+return ( count && <ul className = "card-body-stars u-clearfix">{new Array(count).fill(<li><Star /></li>)}</ul>)
 }
