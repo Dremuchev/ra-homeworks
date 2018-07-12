e80deef260353537d8dbc7ab48204dfbfe96bc51
Andrey <andrey.dremuchev@gmail.com>
2018-07-12 18:57:34 +0300

    func-component-task-2

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function MessageHistory({list}) {
+    const create = (item) => {
+        if (item.type === 'response') {
+            return (<Response from={item.from} message={item} key = {item.id}/>)
+        }
+        if (item.type === 'message') {
+            return (<Message from={item.from} message={item} key = {item.id}/>)
+        }
+        if (item.type === 'typing') {
+            return (<Typing from={item.from} message={item} key = {item.id}/>)
+        }
+    }
+
+    return (
+        list.length ? <ul>{list.map(el => create(el))}</ul> : null
+    )
+}
+
+MessageHistory.defaultProps = {
+    list: []    
+}
