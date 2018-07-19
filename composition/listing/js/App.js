98239bc35cda92163cd89f15106c66b155dbe052
Andrey <andrey.dremuchev@gmail.com>
2018-07-19 12:43:47 +0300

    composition-task-1

--- composition/listing/js/App.js
+++ composition/listing/js/App.js
@@ -1,16 +1,20 @@
 'use strict';
 
-const App = ({items}) => (
+const App = (props) => (
   <main>
-    {items.map(item => {
-      switch(item.type) {
-        case 'unisex':
-          return <Item color="black" item={item} />;
-        case 'male':
-          return <Item color="blue" item={item} />;
-        case 'female':
-          return <Item color="orange" item={item} />;
-      }
-    })}
+    {getItem(props.items)}
   </main>
 );
+
+const getItem = items => {
+  return items.map(item => {
+    switch(item.type) {
+      case 'unisex':
+        return <Item color="black" item={item} />;
+      case 'male':
+        return <Item color="blue" item={item} />;
+      case 'female':
+        return <Item color="orange" item={item} />;
+    }
+  })
+};
