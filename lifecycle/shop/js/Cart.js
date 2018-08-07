8901a95f7f65c178a9893f022c3eb134b73ae7f0
Andrey <andrey.dremuchev@gmail.com>
2018-08-07 16:14:29 +0300

    lifecycle-task-3

--- lifecycle/shop/js/Cart.js
+++ lifecycle/shop/js/Cart.js
@@ -1,9 +1,21 @@
 class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: props.isOpen }
+  }
+
+  componentWillReceiveProps(args) {
+    this.setState({ isOpen: args.items.length !== 0 })
+  }
 
   render() {
-    return (
-      <CartView {...this.props} />
-    );
+    if (this.state.isOpen) {
+      return (
+        <CartView {...this.props} isOpen={this.state.isOpen} />
+      );
+    } else {
+      return null;
+    }
   }
 
 }
